e5a645ce4a60ef3f2fe9a08020663396f98c6431
Mark Walle <mwalle@gmail.com>
2019-02-27 17:24:50 -0800

    patroni postresql templates and deploy

--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -11,6 +11,39 @@ module.exports = (settings)=>{
   const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
   var objects = []
 
+  //Secrets for PGSQL/Patroni
+  //First call will create/generate default values and a template
+  oc.createIfMissing(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/postgresql-secrets.yaml`, {
+    'param':{
+      'NAME': `template.${phases[phase].name}-pgsql-patroni`,
+      'SUFFIX': '',
+      'APP_DB_USERNAME': 'caps',
+      'APP_DB_NAME': 'caps'
+    }
+  }))
+
+  //Second call will create the required object using their respective template (default ones generated above)
+  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/postgresql-secrets.yaml`, {
+    'param':{
+      'NAME': `${phases[phase].name}-pgsql`,
+      'SUFFIX': phases[phase].suffix,
+      'APP_DB_USERNAME': 'caps',
+      'APP_DB_NAME': 'caps'
+    }
+  }))
+
+
+  //Deployment objects for Patroni
+  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/postgresql.yaml`, {
+    'param':{
+      'NAME': `${phases[phase].name}-pgsql`,
+      'SUFFIX': phases[phase].suffix,
+      'INSTANCE': `${phases[phase].name}-pgsql${phases[phase].suffix}`,
+      'IMAGE_STREAM_NAMESPACE': 'bcgov',
+//      'OPENSHIFT_IMAGE_REGISTRY': '172.30.1.1:5000',
+      'IMAGE_STREAM_TAG': 'patroni:v10-stable'
+    }
+  }))
   
   objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/python-deploy.yaml`, {
     'param':{
