39e2793f7079e684fdb41cf9e61d8e78c330ac9d
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2019-02-27 16:35:32 -0800

    Updates

--- .pipeline/lib/deploy.js
+++ .pipeline/lib/deploy.js
@@ -12,21 +12,16 @@ module.exports = (settings)=>{
   var objects = []
 
   
-  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/sso72-x509.yaml`, {
+  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/python-deploy.yaml`, {
     'param':{
       'NAME': phases[phase].name,
       'SUFFIX': phases[phase].suffix,
       'VERSION': phases[phase].tag,
-      'DB_SECRET_NAME':`${phases[phase].name}-pgsql${phases[phase].suffix}`,
-      'DB_SECRET_DATABASE_KEY':'app-db-name',
-      'DB_SECRET_USERNAME_KEY':'app-db-username',
-      'DB_SECRET_PASSWORD_KEY':'app-db-password',
-      'DB_SERVICE_HOST': `${phases[phase].name}-pgsql-master${phases[phase].suffix}`
+      'HOST': phases[phase].host || ''
     }
   }))
 
   oc.applyRecommendedLabels(objects, phases[phase].name, phase, `${changeId}`, phases[phase].instance)
-  
   oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag)
   oc.applyAndDeploy(objects, phases[phase].instance)
 
