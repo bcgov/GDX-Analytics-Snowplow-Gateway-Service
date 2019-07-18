4fa52089332fe8fa23aa32cd863dffd49b3598d6
Mark Walle <mark.walle@gov.bc.ca>
2019-07-18 14:43:25 -0700

    revert options line on .jenkins/.pipeline/lib/build.js

--- .jenkins/.pipeline/lib/build.js
+++ .jenkins/.pipeline/lib/build.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 module.exports = (settings)=>{
   const phases=settings.phases
-  const options = settings.options
   const oc=new OpenShiftClientX(Object.assign({'namespace':phases.build.namespace}, options));
   const phase='build'
   var objects = []
