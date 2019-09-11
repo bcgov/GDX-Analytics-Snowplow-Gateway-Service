18762a1732bbbe61fd55920fff47c6fb6aace572
Mark Walle <mark.walle@gov.bc.ca>
2019-09-11 09:50:05 -0700

    Replace logging approach to use Python's logging library

--- app/app.py
+++ app/app.py
@@ -18,19 +18,27 @@ import json
 import os
 import ssl
 
-# log level
-INFO = True
-DEBUG = True
+# set up logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-def logtime():
-    return datetime.now().strftime("%Y-%m-%d %H:%M:%S:%f")[:-3]
+# create console handler for logs at the INFO level
+# This will be emailed when the cron task runs; formatted to give messages only
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s: %(message)s")
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
 
-def log(l,s):
-    log_line = "{} CAPS [{}]:\t{}".format(logtime(),l,s)
-    if (INFO is not True and l is "INFO") or (DEBUG is not True and l is "DEBUG"):
-        return # supress these if the log levels are not True
-    elif l is "ERROR" or l is "DEBUG" or l is "INFO":
-        print(log_line) # ERROR is not never suppressed
+# create file handler for logs at the DEBUG level
+log_filename = '{0}'.format(os.path.basename(__file__).replace('.py', '.log'))
+log_handler = logging.FileHandler(
+    log_filename, "a", encoding=None, delay="true")
+log_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(levelname)s:%(name)s:%(asctime)s:%(message)s")
+log_handler.setFormatter(formatter)
+logger.addHandler(log_handler)
+
 
 def signal_handler(signal, frame):
     sys.exit(0)
@@ -80,7 +88,7 @@ def single_response_query(sql, execute_tuple, all=False):
             conn.commit()
             cur.close()
     except (Exception, psycopg2.DatabaseError) as e:
-        log("ERROR", "Error retreiving from connection pool {}".format(e))
+        logger.exception("Error retreiving from connection pool")
     finally:
         if conn is not None:
             # Release the connection object and send it back to the pool
@@ -88,12 +96,12 @@ def single_response_query(sql, execute_tuple, all=False):
     return fetch
 
 # Used for on_failure retry backoff; returns the number at index n in the Fibonacci sequence
-def binets_formula(n):
-    sqrt5 = 5 ** 0.5
-    F_n = int((( (1 + sqrt5) ** n - (1 - sqrt5) ** n ) / ( 2 ** n * sqrt5 )))
-    return F_n
+# def binets_formula(n):
+#     sqrt5 = 5 ** 0.5
+#     F_n = int((( (1 + sqrt5) ** n - (1 - sqrt5) ** n ) / ( 2 ** n * sqrt5 )))
+#     return F_n
 
-def call_snowplow(request_id,json_object):
+def call_snowplow(request_id, json_object):
 
     # Use the global emitter and tracker dicts
     global e
@@ -104,11 +112,11 @@ def call_snowplow(request_id,json_object):
 
     # callback for passed calls
     def on_success(successfully_sent_count):
-        log("INFO","Emitter call PASSED on request_id: {}.".format(request_id))
+        logger.info("Emitter call PASSED on request_id: {}.".format(request_id))
         backoff_outside_the_scope = 1 # reset the backoff if this is successful
         # get previous try number, choose larger of 0 or query result and add 1
         try_number = max(i for i in [0,single_response_query("SELECT MAX(try_number) FROM caps.snowplow_calls WHERE request_id = %s ;", (request_id, ))[0]] if i is not None) + 1
-        log("DEBUG","Try number: {}".format(try_number))
+        logger.debug("Try number: {}".format(try_number))
         snowplow_tuple = (
             str(request_id),
             str(200),
@@ -120,7 +128,8 @@ def call_snowplow(request_id,json_object):
             json.dumps(json_object['event_data_json'])
         )
         snowplow_id = single_response_query(snowplow_calls_sql, snowplow_tuple)[0]
-        log("INFO","snowplow call table insertion PASSED on request_id: {} and snowplow_id: {}.".format(request_id, snowplow_id))
+        logger.info("snowplow call table insertion PASSED on request_id: {} and snowplow_id: {}.".format(request_id, snowplow_id))
+        return
 
     # callback for failed calls
     failed_try = 0
@@ -130,9 +139,8 @@ def call_snowplow(request_id,json_object):
         failed_try += 1
 
         # sleep according to the number indexed by failed_try in the fibonacci sequence
-        sleep_time = binets_formula(failed_try)
-        #log("INFO","Emitter call FAILED on request_id {} on try {}. Seconds until re-attempt: {}.".format(request_id,failed_try,sleep_time))
-        log("INFO","Emitter call FAILED on request_id {} on try {}. No re-attempt will be made.".format(request_id,failed_try))
+        # sleep_time = binets_formula(failed_try)
+        logger.info("Emitter call FAILED on request_id {} on try {}. No re-attempt will be made.".format(request_id,failed_try))
 
         # Leaving this sleep delay until inputting after a failed event is ready
         #sleep(sleep_time)
@@ -153,17 +161,17 @@ def call_snowplow(request_id,json_object):
                 json.dumps(json_object['event_data_json'])
             )
             snowplow_id = single_response_query(snowplow_calls_sql, snowplow_tuple)[0]
-            log("INFO","snowplow call table insertion PASSED on request_id: {} and snowplow_id: {}.".format(request_id, snowplow_id))
+            logger.info("snowplow call table insertion PASSED on request_id: {} and snowplow_id: {}.".format(request_id, snowplow_id))
             # Re-attempt the event call by inputting it back to the emitter
             #e[tracker_identifier].input(event)
 
     tracker_identifier = json_object['env'] + "-" + json_object['namespace'] + "-" + json_object['app_id']
-    log("DEBUG","New request with tracker_identifier {}".format(tracker_identifier))
+    logger.debug("New request with tracker_identifier {}".format(tracker_identifier))
 
     # logic to switch between SPM and Production Snowplow.
     # TODO: Fix SSL problem so to omit the anonymization proxy, since we connect from a Gov IP, not a personal machine
     sp_endpoint = os.getenv("SP_ENDPOINT_{}".format(json_object['env'].upper()))
-    log("DEBUG","Using Snowplow Endpoint {}".format(sp_endpoint))
+    logger.debug("Using Snowplow Endpoint {}".format(sp_endpoint))
 
     # Set up the emitter and tracker. If there is already one for this combination of env, namespace, and app-id, reuse it
     # TODO: add error checking
@@ -194,13 +202,13 @@ class RequestHandler(BaseHTTPRequestHandler):
         length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(length).decode('utf-8')
 
-        log("INFO","IP: {}".format(ip_address))
-        log("INFO","HEADERS: {}".format(self.headers))
+        logger.info("IP: {}".format(ip_address))
+        logger.info("HEADERS: {}".format(self.headers))
 
         # Test that the post_data is JSON
         try:
             json_object = json.loads(post_data)
-        except (json.decoder.JSONDecodeError,ValueError) as e:
+        except (json.decoder.JSONDecodeError, ValueError):
             response_code = 400
             self.send_response(response_code, 'POST body is not parsable as JSON.')
             self.send_header('Content-type', 'text/plain')
@@ -212,7 +220,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         # Test that the JSON matches the expeceted schema
         try:
             jsonschema.validate(json_object, post_schema)
-        except (jsonschema.ValidationError, jsonschema.SchemaError ) as e:
+        except (jsonschema.ValidationError, jsonschema.SchemaError):
             response_code = 400
             self.send_response(response_code, 'POST JSON is not compliant with schema.')
             self.send_header('Content-type', 'text/plain')
@@ -252,16 +260,21 @@ class RequestHandler(BaseHTTPRequestHandler):
             )
         request_id = single_response_query(client_calls_sql, post_tuple)[0]
 
-        log("INFO", "Issue Snowplow call: Request ID {}.".format(request_id))
+        logger.info("Issue Snowplow call: Request ID {}.".format(request_id))
         call_snowplow(request_id, json_object)
+        return
 
 # if single_response_query("SELECT 1 ;",None)[0] is not 1:
 #     log("ERROR","There is a problem querying the database.")
 #     sys.exit(1)
 
 
+# ThreadedHTTPServer reference:
+# - https://pymotw.com/2/BaseHTTPServer/index.html#module-BaseHTTPServer
+# - https://stackoverflow.com/a/36439055/5431461
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    pass
+    def server_activate(self):
+        self.socket.listen(20)
 
 
 print("\nGDX Analytics as a Service\n===")
@@ -269,14 +282,19 @@ print("\nGDX Analytics as a Service\n===")
 # Create a threaded PostgreSQL connection pool
 try:
     threaded_postgreSQL_pool = psycopg2.pool.ThreadedConnectionPool(
-        5, 20, user=user, password=password, host=host, database=database)
+        minconn=5,
+        maxconn=20,
+        user=user,
+        password=password,
+        host=host,
+        database=database)
     if(threaded_postgreSQL_pool):
-        log("INFO", "Connection pool created successfully")
+        logger.info("Connection pool created successfully")
 except (Exception, psycopg2.DatabaseError) as e:
-    log("ERROR", "Error while connecting to PostgreSQL {}".format(e))
+    logger.exception("Error while connecting to PostgreSQL")
 
 httpd = ThreadedHTTPServer((address, port), RequestHandler)
-log("INFO", "Listening for POSTs to {} on port {}.".format(address, port))
+logger.info("Listening for POSTs to {} on port {}.".format(address, port))
 httpd.socket = ssl.wrap_socket(
     httpd.socket,
     keyfile="{cert_path}/tls.key".format(cert_path=cert_path),
