92d5f4a8e5c797c82fd0a0e77dd8aef2f0dc878d
Mark Walle <Mark.Walle@gov.bc.ca>
2019-12-06 14:17:40 -0800

    fix

--- app/app.py
+++ app/app.py
@@ -220,7 +220,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         try:
             BaseHTTPRequestHandler.handle(self)
         except SocketError:
-            logger.info("something happened")
+            logging.info("something happened")
             pass
 
     def do_GET(self):
